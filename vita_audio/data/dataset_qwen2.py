6a4f0f1a7205380bd2fc0a1715a6f34c18e79df4
Harsh Shah <iharsh@google.com>
2025-10-16 03:50:29 +0000

    revert data loader change

--- vita_audio/data/dataset_qwen2.py
+++ vita_audio/data/dataset_qwen2.py
@@ -32,8 +32,6 @@ class Qwen2Dataset(BaseDataset):
             **kwargs,
         )
 
-        self.processor["audio"].load_model()
-
         self.default_system_message = "You are a helpful AI assistant."
         self.default_system_message = None
 
@@ -270,6 +268,7 @@ class Qwen2Dataset(BaseDataset):
 
     def __getitem__(self, index):
 
+        self.processor["audio"].load_model()
 
         while True:
             # if True:
